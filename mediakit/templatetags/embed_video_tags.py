91c2e83231f77d5c54c549291bf6925bc009787a
Seth Girsky <sethgirsky@Seths-MBP.lan>
2024-12-11 14:51:48 -0800

    Full first draft done

--- mediakit/templatetags/embed_video_tags.py
+++ mediakit/templatetags/embed_video_tags.py
@@ -8,20 +8,32 @@ register = template.Library()
 
 @register.filter
 def embed_video_url(url):
-    if 'youtube.com' in url:
-        # Extract the video ID from the YouTube URL
-        match = re.search(r'v=([^&]+)', url)
+    # YouTube handling
+    youtube_patterns = [
+        r'(?:youtube\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})',
+    ]
+    
+    for pattern in youtube_patterns:
+        match = re.search(pattern, url)
         if match:
             video_id = match.group(1)
             return f'https://www.youtube.com/embed/{video_id}'
-    elif 'youtu.be' in url:
-        # Handle shortened YouTube URLs
-        match = re.search(r'youtu\.be/([^?]+)', url)
-        if match:
-            video_id = match.group(1)
-            return f'https://www.youtube.com/embed/{video_id}'
-    # Add more conditions for other platforms if needed
-    return url  # Fallback to the original URL
+    
+    # Instagram handling
+    instagram_pattern = r'(?:instagram.com/(?:p|reel)/([^/]+))'
+    match = re.search(instagram_pattern, url)
+    if match:
+        post_id = match.group(1)
+        return f'https://www.instagram.com/p/{post_id}/embed'
+    
+    # TikTok handling
+    tiktok_pattern = r'(?:tiktok\.com/(?:@[^/]+/video|v)/(\d+))'
+    match = re.search(tiktok_pattern, url)
+    if match:
+        video_id = match.group(1)
+        return f'https://www.tiktok.com/embed/v2/{video_id}'
+    
+    return url  # Fallback to original URL if no matches
 
 
 
